0a21f1a58a522852d1082b71888bebbd24ade768
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-11-21 21:09:27 +0200

    fixed problems with details.css and added control form to sign in page

--- ubuy/src/components/user/user-management/SignIn.jsx
+++ ubuy/src/components/user/user-management/SignIn.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 import "./UserManagement.css";
 
+import { useForm } from "../../../core/hooks/useForm";
+
 const SignIn = () => {
+  const { values, onChange, onSubmit } = useForm({
+    email: "",
+    password: "",
+  });
+
   return (
-    <div className="auth-form">
+    <form className="auth-form" onSubmit={onSubmit}>
       <div className="auth-form-info">
         <div className="auth-form-title">Sign In</div>
         <div className="auth-form-desc">
@@ -12,13 +19,30 @@ const SignIn = () => {
       </div>
       <div className="input-field">
         <i className="fa-solid fa-envelope icon"></i>
-        <input type="text" className="auth-input" placeholder="Email" />
+        <input
+          type="text"
+          className="auth-input"
+          placeholder="Email"
+          name="email"
+          onChange={onChange}
+          value={values["email"]}
+        />
       </div>
       <div className="input-field">
         <i className="fa-solid fa-lock icon"></i>
-        <input type="text" className="auth-input" placeholder="Password" />
+        <input
+          type="text"
+          className="auth-input"
+          placeholder="Password"
+          name="password"
+          onChange={onChange}
+          value={values["password"]}
+        />
       </div>
-      <button className="auth-button">Sign In</button>
+
+      <button className="auth-button" type="submit">
+        Sign In
+      </button>
 
       <div className="splitter">
         <span>OR</span>
@@ -28,7 +52,7 @@ const SignIn = () => {
         <span>Don&apos;t have an account, yet?</span>
         <Link to="/sign-up">Sign Up</Link>
       </div>
-    </div>
+    </form>
   );
 };
 
