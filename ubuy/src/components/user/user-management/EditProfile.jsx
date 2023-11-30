68b1941bfc7b3129232c0387c9ca10a7c57c04d4
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-11-30 22:55:11 +0200

    made edit your profile component work

--- ubuy/src/components/user/user-management/EditProfile.jsx
+++ ubuy/src/components/user/user-management/EditProfile.jsx
@@ -1,8 +1,70 @@
+import { useEffect, useState } from "react";
 import "./UserManagement.css";
+import { useNavigate, useParams } from "react-router-dom";
+
+import * as userService from "../../../core/services/userService";
+import {
+  SendErrorNotification,
+  SendSuccessNotification,
+} from "../../../core/notifications/notifications";
+import { parseError } from "../../../core/lib/errorParser";
 
 const EditProfile = () => {
+  const { profileId } = useParams();
+  const [user, setUser] = useState({
+    _id: "",
+    email: "",
+    username: "",
+    firstName: "",
+    lastName: "",
+    pfpUrl: "",
+    role: "",
+    products: [],
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    userService
+      .getProfileInfo(profileId)
+      .then(setUser)
+      .catch((error) => {
+        console.log(error);
+        SendErrorNotification("User does not exist.");
+
+        navigate("/");
+      });
+  }, [profileId]);
+
+  const onProfileEdit = async (e) => {
+    e.preventDefault();
+
+    // @ts-ignore
+    const values = Object.fromEntries(new FormData(e.currentTarget));
+
+    try {
+      await userService.editProfileInfo(profileId, values);
+
+      SendSuccessNotification("Profile information updated successfully.");
+
+      navigate(`/users/${profileId}`);
+    } catch (error) {
+      let errors = parseError(error);
+
+      errors.forEach((err) => {
+        SendErrorNotification(err);
+      });
+    }
+  };
+
+  const onChange = (e) => {
+    setUser((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
-    <div className="auth-form two-col">
+    <form className="auth-form two-col" onSubmit={onProfileEdit}>
       <div className="auth-form-info">
         <div className="auth-form-title">Edit Your Profile</div>
         <div className="auth-form-desc">
@@ -11,33 +73,47 @@ const EditProfile = () => {
       </div>
       <div className="input-field two-col-input">
         <i className="fa-solid fa-id-badge icon"></i>
-        <input type="text" className="auth-input" placeholder="First Name" />
-      </div>
-      <div className="input-field two-col-input">
-        <i className="fa-solid fa-id-badge icon"></i>
-        <input type="text" className="auth-input" placeholder="Last Name" />
-      </div>
-      <div className="input-field two-col-input">
-        <i className="fa-solid fa-envelope icon"></i>
-        <input type="text" className="auth-input" placeholder="Email" />
-      </div>
-      <div className="input-field two-col-input">
-        <i className="fa-solid fa-user icon"></i>
-        <input type="text" className="auth-input" placeholder="Username" />
-      </div>
-      <div className="input-field two-col-input">
-        <i className="fa-solid fa-eye icon toggle-password"></i>
-        <i className="fa-solid fa-lock icon"></i>
         <input
           type="text"
           className="auth-input"
-          placeholder="Current Password"
+          placeholder="First Name"
+          name="firstName"
+          value={user.firstName}
+          onChange={onChange}
         />
       </div>
       <div className="input-field two-col-input">
-        <i className="fa-solid fa-eye icon toggle-password"></i>
-        <i className="fa-solid fa-lock icon"></i>
-        <input type="text" className="auth-input" placeholder="New Password" />
+        <i className="fa-solid fa-id-badge icon"></i>
+        <input
+          type="text"
+          className="auth-input"
+          placeholder="Last Name"
+          name="lastName"
+          value={user.lastName}
+          onChange={onChange}
+        />
+      </div>
+      <div className="input-field two-col-input">
+        <i className="fa-solid fa-envelope icon"></i>
+        <input
+          type="text"
+          className="auth-input"
+          placeholder="Email"
+          name="email"
+          value={user.email}
+          onChange={onChange}
+        />
+      </div>
+      <div className="input-field two-col-input">
+        <i className="fa-solid fa-user icon"></i>
+        <input
+          type="text"
+          className="auth-input"
+          placeholder="Username"
+          name="username"
+          value={user.username}
+          onChange={onChange}
+        />
       </div>
       <div className="input-field two-col-input">
         <i className="fa-solid fa-image icon"></i>
@@ -45,19 +121,33 @@ const EditProfile = () => {
           type="text"
           className="auth-input"
           placeholder="Profile Picture URL"
+          name="pfpUrl"
+          value={user.pfpUrl}
+          onChange={onChange}
         />
       </div>
       <div className="input-field two-col-input">
         <i className="fa-solid fa-cart-shopping icon"></i>
-        <select name="role" id="role" className="auth-input">
+        <select
+          name="role"
+          id="role"
+          className="auth-input"
+          value={user.role}
+          onChange={onChange}
+        >
           <option value="buyer">Buyer</option>
-          <option value="seler">Seler</option>
-          <option value="both">Both</option>
+          <option value="seller">Seller</option>
+          <option value="admin" disabled>
+            Admin
+          </option>
+          <option value="owner" disabled>
+            Owner
+          </option>
         </select>
       </div>
 
       <button className="auth-button">Edit Profile</button>
-    </div>
+    </form>
   );
 };
 
