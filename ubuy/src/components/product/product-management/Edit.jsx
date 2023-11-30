979627f5268a2b53c753ef02af1f2e072c1417c1
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-11-30 22:55:03 +0200

    made editing your product component work

--- ubuy/src/components/product/product-management/Edit.jsx
+++ ubuy/src/components/product/product-management/Edit.jsx
@@ -1,14 +1,18 @@
 import { useNavigate, useParams } from "react-router-dom";
 import "./ProductManagement.css";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import * as productService from "../../../core/services/productService";
 import { parseError } from "../../../core/lib/errorParser";
-import { SendErrorNotification } from "../../../core/notifications/notifications";
-import { useForm } from "../../../core/hooks/useForm";
+import {
+  SendErrorNotification,
+  SendSuccessNotification,
+} from "../../../core/notifications/notifications";
+import AuthContext from "../../../core/contexts/authContext";
 
 const Edit = () => {
   const navigate = useNavigate();
   const { productId } = useParams();
+  const { userId } = useContext(AuthContext);
   const [product, setProduct] = useState({
     name: "",
     quantity: "",
@@ -16,6 +20,7 @@ const Edit = () => {
     category: "",
     imageURL: "",
     description: "",
+    owner: {},
   });
 
   useEffect(() => {
@@ -24,14 +29,58 @@ const Edit = () => {
     });
   }, [productId]);
 
-  const onProductEdit = async (values) => {};
+  if (userId != product.owner._id) {
+    navigate(`/market/${productId}/details`);
+  }
 
-  const { values, onChange, onSubmit } = useForm(onProductEdit, product);
+  const onProductEdit = async (e) => {
+    e.preventDefault();
 
-  console.log(product, values);
+    // @ts-ignore
+    const values = Object.fromEntries(new FormData(e.currentTarget));
+
+    // Values Validation
+    if (values.name.length < 5) {
+      return SendErrorNotification(
+        `Product name should be at least 5 characters long`
+      );
+    }
+    if (values.description.length < 20) {
+      return SendErrorNotification(
+        `Product description should be at least 20 characters long`
+      );
+    }
+    if (values.quantity < 1) {
+      return SendErrorNotification(`Product quantity should be at least 1`);
+    }
+    if (values.price < 1) {
+      return SendErrorNotification(`Product price should be at least 1`);
+    }
+
+    try {
+      await productService.editProduct(productId, values);
+
+      SendSuccessNotification("Product listing updated successfully.");
+
+      navigate(`/market/${productId}/details`);
+    } catch (error) {
+      let errors = parseError(error);
+
+      errors.forEach((err) => {
+        SendErrorNotification(err);
+      });
+    }
+  };
+
+  const onChange = (e) => {
+    setProduct((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   return (
-    <form className="create-form two-col" onSubmit={onSubmit}>
+    <form className="create-form two-col" onSubmit={onProductEdit}>
       <div className="create-form-info">
         <div className="create-form-title">Edit Listing</div>
         <div className="create-form-desc">
@@ -46,29 +95,32 @@ const Edit = () => {
           className="auth-input"
           placeholder="Product Name"
           name="name"
-          value={values.name}
+          value={product.name}
           onChange={onChange}
         />
       </div>
       <div className="input-field-create two-col-input-create">
         <i className="fa-solid fa-arrow-up-wide-short icon"></i>
         <input
-          type="text"
+          type="number"
           className="auth-input"
           placeholder="Quantity"
           name="quantity"
-          value={values.quantity}
+          min={1}
+          max={50}
+          value={product.quantity}
           onChange={onChange}
         />
       </div>
       <div className="input-field-create two-col-input-create">
         <i className="fa-solid fa-money-bill-wave icon"></i>
         <input
-          type="text"
+          type="number"
           className="auth-input"
           placeholder="Price"
           name="price"
-          value={values.price}
+          min={1}
+          value={product.price}
           onChange={onChange}
         />
       </div>
@@ -79,7 +131,7 @@ const Edit = () => {
           id="category"
           className="auth-input"
           placeholder="Category"
-          value={values.category}
+          value={product.category}
           onChange={onChange}
         >
           <option value="home">Home</option>
@@ -95,7 +147,7 @@ const Edit = () => {
           className="auth-input"
           placeholder="Image URL"
           name="imageURL"
-          value={values.imageURL}
+          value={product.imageURL}
           onChange={onChange}
         />
       </div>
@@ -106,7 +158,7 @@ const Edit = () => {
           cols={30}
           rows={9}
           placeholder="Breif description of your product..."
-          value={values.description}
+          value={product.description}
           onChange={onChange}
         ></textarea>
       </div>
