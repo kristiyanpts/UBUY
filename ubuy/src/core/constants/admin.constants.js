2ae1b7f16af21dfa9cbcf181cfdaabf48d3d29ad
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-12-04 11:42:58 +0200

    added new profile to admin list

--- ubuy/src/core/constants/admin.constants.js
+++ ubuy/src/core/constants/admin.constants.js
@@ -1,3 +1,3 @@
 // Add your id after creating your account and change your role to admin to access the admin panel
 
-export const ADMIN_LIST = ["65635c52a428d44a009074de"];
+export const ADMIN_LIST = ["656d8ce1ae654a8c8c01e652"];
